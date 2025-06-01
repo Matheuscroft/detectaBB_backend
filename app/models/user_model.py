7dd70ca503bd964b824225f3120c1bc19c2f9e40
Matheus OS <matheus.sdoliveira@gmail.com>
2025-05-31 21:56:29 -0300

    user model hash change

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    senha_hash = db.Column(db.String(128), nullable=False)
+    senha_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, senha):
         self.senha_hash = generate_password_hash(senha)
